6e45d405deee70e49026ae6b6afe1344bbcf34a1
HyeonminNam <goisugn@gmail.com>
2020-10-27 09:57:01 +0900

    Bertsum 추가 코드 커밋

---
+++ BertSum/src/prepro/customized.py
@@ -0,0 +1,215 @@
+import gc
+import glob
+import hashlib
+import itertools
+import json
+import os
+import re
+import subprocess
+import time
+from os.path import join as pjoin
+
+import torch
+from multiprocess import Pool
+from pytorch_pretrained_bert import BertTokenizer
+
+from others.logging import logger
+from others.utils import clean
+from prepro.utils import _get_word_ngrams
+
+import pandas as pd
+
+from konlpy.tag import Kkma
+
+
+
+
+def cal_rouge(evaluated_ngrams, reference_ngrams):
+    reference_count = len(reference_ngrams)
+    evaluated_count = len(evaluated_ngrams)
+
+    overlapping_ngrams = evaluated_ngrams.intersection(reference_ngrams)
+    overlapping_count = len(overlapping_ngrams)
+
+    if evaluated_count == 0:
+        precision = 0.0
+    else:
+        precision = overlapping_count / evaluated_count
+
+    if reference_count == 0:
+        recall = 0.0
+    else:
+        recall = overlapping_count / reference_count
+
+    f1_score = 2.0 * ((precision * recall) / (precision + recall + 1e-8))
+    return {"f": f1_score, "p": precision, "r": recall}
+
+
+def combination_selection(doc_sent_list, abstract_sent_list, summary_size):
+    def _rouge_clean(s):
+        return re.sub(r'[^a-zA-Z0-9가-힣 ]', '', s)
+
+    max_rouge = 0.0
+    max_idx = (0, 0)
+    abstract = sum(abstract_sent_list, [])
+    abstract = _rouge_clean(' '.join(abstract)).split()
+    sents = [_rouge_clean(' '.join(s)).split() for s in doc_sent_list]
+    evaluated_1grams = [_get_word_ngrams(1, [sent]) for sent in sents]
+    reference_1grams = _get_word_ngrams(1, [abstract])
+    evaluated_2grams = [_get_word_ngrams(2, [sent]) for sent in sents]
+    reference_2grams = _get_word_ngrams(2, [abstract])
+
+    impossible_sents = []
+    for s in range(summary_size + 1):
+        combinations = itertools.combinations([i for i in range(len(sents)) if i not in impossible_sents], s + 1)
+        for c in combinations:
+            candidates_1 = [evaluated_1grams[idx] for idx in c]
+            candidates_1 = set.union(*map(set, candidates_1))
+            candidates_2 = [evaluated_2grams[idx] for idx in c]
+            candidates_2 = set.union(*map(set, candidates_2))
+            rouge_1 = cal_rouge(candidates_1, reference_1grams)['f']
+            rouge_2 = cal_rouge(candidates_2, reference_2grams)['f']
+
+            rouge_score = rouge_1 + rouge_2
+            if (s == 0 and rouge_score == 0):
+                impossible_sents.append(c[0])
+            if rouge_score > max_rouge:
+                max_idx = c
+                max_rouge = rouge_score
+    return sorted(list(max_idx))
+
+
+def greedy_selection(doc_sent_list, abstract_sent_list, summary_size):
+    def _rouge_clean(s):
+        return re.sub(r'[^a-zA-Z0-9가-힣 ]', '', s)
+
+    max_rouge = 0.0
+    abstract = sum(abstract_sent_list, [])
+    abstract = _rouge_clean(' '.join(abstract)).split()
+    sents = [_rouge_clean(' '.join(s)).split() for s in doc_sent_list]
+    evaluated_1grams = [_get_word_ngrams(1, [sent]) for sent in sents]
+    reference_1grams = _get_word_ngrams(1, [abstract])
+    evaluated_2grams = [_get_word_ngrams(2, [sent]) for sent in sents]
+    reference_2grams = _get_word_ngrams(2, [abstract])
+
+    selected = []
+    for s in range(summary_size):
+        cur_max_rouge = max_rouge
+        cur_id = -1
+        for i in range(len(sents)):
+            if (i in selected):
+                continue
+            c = selected + [i]
+            candidates_1 = [evaluated_1grams[idx] for idx in c]
+            candidates_1 = set.union(*map(set, candidates_1))
+            candidates_2 = [evaluated_2grams[idx] for idx in c]
+            candidates_2 = set.union(*map(set, candidates_2))
+            rouge_1 = cal_rouge(candidates_1, reference_1grams)['f']
+            rouge_2 = cal_rouge(candidates_2, reference_2grams)['f']
+            rouge_score = rouge_1 + rouge_2
+            if rouge_score > cur_max_rouge:
+                cur_max_rouge = rouge_score
+                cur_id = i
+        if (cur_id == -1):
+            return selected
+        selected.append(cur_id)
+        max_rouge = cur_max_rouge
+
+    return sorted(selected)
+
+
+class BertData():
+    def __init__(self, args):
+        self.args = args
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-uncased', do_lower_case=True)
+        self.sep_vid = self.tokenizer.vocab['[SEP]']
+        self.cls_vid = self.tokenizer.vocab['[CLS]']
+        self.pad_vid = self.tokenizer.vocab['[PAD]']
+
+    def preprocess(self, src, tgt, oracle_ids):
+    
+        if (len(src) == 0):
+            return None
+
+
+        labels = [0] * len(src)
+        for l in oracle_ids:
+            labels[l] = 1
+
+        if (len(labels) == 0):
+            return None
+
+        src_txt = [' '.join(sent) for sent in src]
+        # text = [' '.join(ex['src_txt'][i].split()[:self.args.max_src_ntokens]) for i in idxs]
+        # text = [_clean(t) for t in text]
+        text = ' [SEP] [CLS] '.join(src_txt)
+        src_subtokens = self.tokenizer.tokenize(text)
+        src_subtokens = src_subtokens[:510]
+        src_subtokens = ['[CLS]'] + src_subtokens + ['[SEP]']
+
+        src_subtoken_idxs = self.tokenizer.convert_tokens_to_ids(src_subtokens)
+        _segs = [-1] + [i for i, t in enumerate(src_subtoken_idxs) if t == self.sep_vid]
+        segs = [_segs[i] - _segs[i - 1] for i in range(1, len(_segs))]
+        segments_ids = []
+        for i, s in enumerate(segs):
+            if (i % 2 == 0):
+                segments_ids += s * [0]
+            else:
+                segments_ids += s * [1]
+        cls_ids = [i for i, t in enumerate(src_subtoken_idxs) if t == self.cls_vid]
+        labels = labels[:len(cls_ids)]
+
+        tgt_txt = '<q>'.join([' '.join(tt) for tt in tgt])
+        return src_subtoken_idxs, labels, segments_ids, cls_ids, src_txt, tgt_txt
+
+
+def format_to_bert(args):
+    if (args.dataset != ''):
+        datasets = [args.dataset]
+    else:
+        datasets = ['train', 'test']
+    for corpus_type in datasets:
+        a_lst = []
+        for json_f in glob.glob(pjoin(args.raw_path,  '*' + corpus_type + '.*.json')):
+            real_name = json_f.split('/')[-1]
+            
+            a_lst.append((json_f, args, pjoin(args.save_path, real_name[12:-4]+'bert.pt')))
+        print(a_lst)
+        pool = Pool(args.n_cpus)
+        for d in pool.imap(_format_to_bert, a_lst):
+            pass
+
+        pool.close()
+        pool.join()
+
+
+def _format_to_bert(params):
+    json_file, args, save_file = params
+    if (os.path.exists(save_file)):
+        logger.info('Ignore %s' % save_file)
+        return
+
+    bert = BertData(args)
+
+    logger.info('Processing %s' % json_file)
+    jobs = json.load(open(json_file))
+    datasets = []
+    for d in jobs:
+        source, tgt = d['src'], d['tgt']
+        if (args.oracle_mode == 'greedy'):
+            oracle_ids = greedy_selection(source, tgt, 3)
+        elif (args.oracle_mode == 'combination'):
+            oracle_ids = combination_selection(source, tgt, 3)
+        b_data = bert.preprocess(source, tgt, oracle_ids)
+        if (b_data is None):
+            continue
+        indexed_tokens, labels, segments_ids, cls_ids, src_txt, tgt_txt = b_data
+        b_data_dict = {"src": indexed_tokens, "labels": labels, "segs": segments_ids, 'clss': cls_ids,
+                       'src_txt': src_txt, "tgt_txt": tgt_txt}
+        datasets.append(b_data_dict)
+    logger.info('Saving to %s' % save_file)
+    torch.save(datasets, save_file)
+    datasets = []
+    gc.collect()
+
+
