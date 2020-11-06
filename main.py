9515bd7812e90391bd276febd31d660ce0f156d7
HyeonminNam <goisugn@gmail.com>
2020-11-06 15:38:15 +0900

    전체 프로세스 포함한 코드 작성 완료(Textrank까지 구현, Bertsum 추가 예정)

--- main.py
+++ main.py
@@ -1,43 +1,36 @@
-# from STT import SpeechtoText
+from STT.las_pytorch import YTT
 from TS.Summarize import Textrank
-from Youtube_to_wav import Youtube_to_Wav
 
 # process
 # youtube(link) to speech(wav)
 # speech(wav) to text
 # text summarize -> result
 
-class SpeechtoSummarize():
+class YoutubetoSummary():
     def __init__(self):
-        # self.stt_model = SpeechtoText()
-        self.ts_model = Textrank()
-        self.youtube_to_wav = Youtube_to_Wav()
+        self.textrank = Textrank()
+        self.ytt = YTT
 
-    def yts(self, link, wav_path='./test.wav'):
-        self.youtube_to_wav.to_wav(link)
+    def youtube_to_text(self, link):
+        self.ytt.youtube_to_wav(link)
+        result = self.ytt.wav_to_text()
+        return result
 
-    def stt(self, wav_path):
-        # text = self.stt_model.to_text(wav_path)
-        text = '''안녕하세요. 저희 팀은 STS 모델을 제작하고 있습니다.
-        STT 모델과 TS 모델을 합쳐서 만드는 모델입니다. 현재 TS 모델은 Textrank로 구현하였습니다. 
-        STT 모델은 아직 미완성입니다. STT 모델이 완성되면 추가하도록 하겠습니다. 
-        TS 모델의 딥러닝 모델도 구현 중입니다. 완전체가 갖춰질 때까지 기다려주세요.
-        곧 찾아뵙겠습니다. 감사합니다.'''
-        return text
-
-    def ts(self, text, model='gensim'):
-        return self.ts_model.summarizer(text, model)
+    def text_to_summary(self, text, option):
+        return self.textrank.summarizer(text, option)
     
-    def sts(self, link, wav_path='./test.wav', textrank = 'gensim'):
-        self.yts(link, wav_path)
-        print('youtube 영상을 wav 파일로 저장하였습니다(경로: {})'.format(wav_path))
-        text = self.stt(wav_path)
-        print(text)
-        summarize_result = self.ts(text, textrank)
-        return summarize_result
+    def youtube_to_summary(self, link, ts = 'tr', tr_option='krwordrank'):
+        text_result = self.youtube_to_text(link)
+        for x in text_result:
+            if ts == 'tr':
+                summarize_result = self.text_to_summary(x, tr_option)
+            # bertsum 추가 예정
+            else:
+                pass
+            for summarize in summarize_result:
+                print(summarize)
 
 if __name__ == '__main__':
-    test_link = 'https://www.youtube.com/watch?v=tS_rgotOLe4'
-    model = SpeechtoSummarize()
-    test_result = model.sts(test_link, './test.wav','gensim')
-    print(test_result)
+    test_link = 'https://youtu.be/3IBycan2RS8'
+    yts = YoutubetoSummary()
+    yts.youtube_to_summary(test_link)
