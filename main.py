6fe34c0ad8277675d7868a2c74d6756e166c5f59
HyeonminNam <goisugn@gmail.com>
2020-11-08 02:27:35 +0900

    전체 프로세스 코드에 BertSum 모델 반영

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from STT.las_pytorch import YTT
 from TS.Summarize import Textrank
+from BertSum.src.final import data_create, do_BertSum
 
 # process
 # youtube(link) to speech(wav)
@@ -16,21 +17,29 @@ class YoutubetoSummary():
         result = self.ytt.wav_to_text()
         return result
 
-    def text_to_summary(self, text, option):
+    def textrank_summary(self, text, option):
         return self.textrank.summarizer(text, option)
+    
+    def bert_summary(self, text):
+        data_create(text)
+        summary = do_BertSum()
+        return summary
     
     def youtube_to_summary(self, link, ts = 'tr', tr_option='krwordrank'):
         text_result = self.youtube_to_text(link)
         for x in text_result:
             if ts == 'tr':
-                summarize_result = self.text_to_summary(x, tr_option)
+                summarize_result = self.textrank_summary(x, tr_option)
             # bertsum 추가 예정
-            else:
-                pass
+            elif ts == 'bs':
+                summarize_result = self.bert_summary(x)
+            print('-'*50+'요약 결과'+'-'*50)
             for summarize in summarize_result:
                 print(summarize)
+            print('-'*109)
 
 if __name__ == '__main__':
-    test_link = 'https://youtu.be/3IBycan2RS8'
+    # 'https://youtu.be/3IBycan2RS8'
+    test_link = input('유튜브 링크를 입력해 주세요: ')
     yts = YoutubetoSummary()
-    yts.youtube_to_summary(test_link)
+    yts.youtube_to_summary(test_link, ts='bs')
